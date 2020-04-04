d6df13efdb8dcdb954405d7a7d220b600a1d8bdf
Tomonori Shimomura <shimomut@amazon.com>
2020-04-03 19:11:20 -0700

    Eliminate FutureWarning ("FutureWarning: Addition/subtraction of integers and integer-arrays to Timestamp is deprecated, will be removed in a future version.  Instead of adding/subtracting `n`, use `n * self.freq`")

--- notebooks/common/util/fcst_utils.py
+++ notebooks/common/util/fcst_utils.py
@@ -97,17 +97,18 @@ def plot_forecasts(fcsts, exact, freq = '1H', forecastHorizon=24, time_back = 80
     p50 = pd.DataFrame(fcsts['Forecast']['Predictions']['p50'])
     p90 = pd.DataFrame(fcsts['Forecast']['Predictions']['p90'])
     pred_int = p50['Timestamp'].apply(lambda x: pd.Timestamp(x))
-    fcst_start_date = pred_int[0]
+    fcst_start_date = pred_int.iloc[0]
+    fcst_end_date = pred_int.iloc[-1]
     time_int = exact['timestamp'].apply(lambda x: pd.Timestamp(x))
     plt.plot(time_int[-time_back:],exact['target'].values[-time_back:], color = 'r')
-    plt.plot(pred_int, p50['Value'].values, color = 'k');
+    plt.plot(pred_int, p50['Value'].values, color = 'k')
     plt.fill_between(p50['Timestamp'].values, 
                      p10['Value'].values,
                      p90['Value'].values,
                      color='b', alpha=0.3);
-    plt.axvline(x=pd.Timestamp(fcst_start_date), linewidth=3, color='g', ls='dashed');
-    plt.axvline(x=pd.Timestamp(fcst_start_date, freq)+forecastHorizon-1, linewidth=3, color='g', ls='dashed');
-    plt.xticks(rotation=30);
+    plt.axvline(x=pd.Timestamp(fcst_start_date), linewidth=3, color='g', ls='dashed')
+    plt.axvline(x=pd.Timestamp(fcst_end_date), linewidth=3, color='g', ls='dashed')
+    plt.xticks(rotation=30)
     plt.legend(['Target', 'Forecast'], loc = 'lower left')
 
 
